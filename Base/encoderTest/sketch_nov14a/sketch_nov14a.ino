c779b41d9dd422ad8c56f3459b879fe9f28c972b
CasKK <82200666+CasKK@users.noreply.github.com>
2025-11-17 14:33:09 +0100

    Encoder test

--- Base/encoderTest/sketch_nov14a/sketch_nov14a.ino
+++ Base/encoderTest/sketch_nov14a/sketch_nov14a.ino
@@ -1,8 +1,16 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-int ENCODER_A = 9;
+Servo myServo;  // create a servo object
 
-int encoder_Value = 0;
+
+
+
+int ENCODER_A = 2;
+
+unsigned long encoderTime = 0;
+
+long encoder_Value = 0;
 
 
 void encoderAFunc();
@@ -10,16 +18,22 @@ void encoderAFunc();
 void setup() {
   Serial.begin(115200);
   pinMode(ENCODER_A, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderAFunc, CHANGE);
+  myServo.attach(6);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderAFunc, RISING);
 }
 
 void loop() {
-  Serial.print(">encoderValue:");
+  Serial.print("  >DigitalValue:");
+  Serial.print(digitalRead(ENCODER_A));
+  Serial.print("  >EncoderValue:");
   Serial.println(encoder_Value);
+  myServo.write(135);
   delay(1);
 }
 
 void encoderAFunc() {
-  encoder_Value += 1;
+  if (micros() - encoderTime > 500) {
+    encoder_Value += 1;
+  }
+  encoderTime = micros();
 }
