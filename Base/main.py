b797342d747409922df81349d7ea8922dce02c17
CasKK <82200666+CasKK@users.noreply.github.com>
2025-11-28 10:13:41 +0100

    FOLDER RENAME!

--- Base/main.py
+++ Base/main.py
@@ -1,13 +1,13 @@
 from multiprocessing import Process, Queue
 import time
 #from M111_perception import M111_perception
-from MR113_Position import Coordinates
+from M113_position import Coordinates
 from M121_logic import M121_logic
 
 if __name__ == "__main__":
 
     q_m113_to_m121 = Queue()
-    
+
     m113 = Process(target=Coordinates.run, args=(q_m113_to_m121,))
     m121 = Process(target=M121_logic.run, args=(q_m113_to_m121,))
 
