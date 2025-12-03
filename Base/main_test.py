e9395d1d0a620429fe22d13afcb0f9df457a564c
CasKK <82200666+CasKK@users.noreply.github.com>
2025-12-03 14:46:38 +0100

    Test files

---
+++ Base/main_test.py
@@ -0,0 +1,29 @@
+from multiprocessing import Process, Queue
+import time
+#from M111_cam import ConeDetectionv3
+from M113_position import Coordinates_test
+from M121_logic import M121_logic_current_test
+
+if __name__ == "__main__":
+
+    #q_m111_to_m113 = Queue()
+    q_m113_to_m121 = Queue()
+
+    #m111 = Process(target=ConeDetectionv3.run, args=(q_m111_to_m113,))
+    m113 = Process(target=Coordinates_test.run, args=(q_m113_to_m121,))
+    m121 = Process(target=M121_logic_current_test.run, args=(q_m113_to_m121,))
+
+    #m111.start()
+    m113.start()
+    m121.start()
+
+    print("CTRL + C = kEYboArDInTerRuPt")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("kIlLiNg!")
+        #m111.terminate()
+        m113.terminate()
+        m121.terminate()
