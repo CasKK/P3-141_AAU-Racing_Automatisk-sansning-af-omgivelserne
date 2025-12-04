36dc3cc55564ccb18184e771b68161a9cb9148f7
CasKK <82200666+CasKK@users.noreply.github.com>
2025-12-04 15:51:52 +0100

    Update serial_manager_test.py

--- Base/serial_manager_test.py
+++ Base/serial_manager_test.py
@@ -3,31 +3,40 @@ import time
 from multiprocessing import Queue
 
 def run(read_queue, write_queue):
-    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1)
+    # ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1)
     time.sleep(2)  # allow Arduino reset
 
+    
+    lastTime = 0
+    encoderCounts = 0
+    degrees = 0
 
     while True:
         # --- READ ---
-        line = ser.readline().decode(errors='ignore').strip()
-        if line:
-            if "," in line:
-                try:
-                    angle_str, count_str = line.split(',')
-                    angle = float(angle_str)
-                    count = int(count_str)
-                    read_queue.put((angle, count))
-                    print("Angle:", angle, "Count:", count)
-                except ValueError:
-                    print("Bad line:", line)
-            else:
-                print("skrewed line:", line)
+        # line = ser.readline().decode(errors='ignore').strip()
+        # if line:
+        #     if "," in line:
+        #         try:
+        #             angle_str, count_str = line.split(',')
+        #             angle = float(angle_str)
+        #             count = int(count_str)
+        #             read_queue.put((angle, count))
+        #             print("Angle:", angle, "Count:", count)
+        #         except ValueError:
+        #             print("Bad line:", line)
+        #     else:
+        #         print("skrewed line:", line)
         
+        if lastTime + 0.01 < time.time():
+            read_queue.put((degrees, encoderCounts))
+            degrees += 0.1
+            encoderCounts += 10
+            lastTime = time.time()
 
         # --- WRITE ---
         while not write_queue.empty():
             message = write_queue.get()
-            ser.write((message + '\n').encode('utf-8'))
+            # ser.write((message + '\n').encode('utf-8'))
             print(f"messageOutToArduino: {message}")
 
         time.sleep(0.001)
