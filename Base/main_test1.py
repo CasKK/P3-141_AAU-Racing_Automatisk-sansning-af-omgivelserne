1c4b0593380cd546e043df85881a178408990873
CasKK <82200666+CasKK@users.noreply.github.com>
2025-12-11 15:37:06 +0100

    zeroFound

---
+++ Base/main_test1.py
@@ -0,0 +1,30 @@
+from multiprocessing import Process, Queue
+import time
+# from M111_cam import ConeDetectionv3
+from M113_position import Coordinates_test4
+from M121_logic import M121_logic
+
+if __name__ == "__main__":
+
+    # q_m111_to_m113 = Queue()
+    q_m113_to_m121 = Queue()
+    q_serial_write = Queue() # Serial que til afsendelse "kun på m121"
+    
+    # m111 = Process(target=ConeDetectionv3.run, args=(q_m111_to_m113,))
+    m113 = Process(target=Coordinates_test4.run, args=(q_m113_to_m121,)) # q_m111_to_m113, 
+    m121 = Process(target=M121_logic.run, args=(q_m113_to_m121, q_serial_write))
+
+    # m111.start()
+    m113.start()
+    m121.start()
+
+    print("CTRL + C = kEYboArDInTerRuPt")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("kIlLiNg!")
+        #m111.terminate()
+        m113.terminate()
+        m121.terminate()
--- Base/serial_manager.py
+++ Base/serial_manager.py
@@ -5,6 +5,7 @@ import csv
 import json
 import datetime
 
+zeroFound = False
 
 def run(read_queue, write_queue):
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
@@ -20,16 +21,19 @@ def run(read_queue, write_queue):
                         angle_str, count_str = line.split(',')
                         angle = float(angle_str) 
                         count = float(count_str)
-                        if read_queue.qsize() >= 5:
-                            try:
-                                read_queue.get_nowait()  # remove oldest item
-                            except:
-                                pass
-                        read_queue.put((angle, count))
-                        print("Angle:", angle, "Count:", count)
-                        timestamp = time.time()
-                        writer.writerow([timestamp, angle, count])
-                        f.flush()
+                        if count < 1:
+                            zeroFound = True
+                        if zeroFound:
+                            if read_queue.qsize() >= 5:
+                                try:
+                                    read_queue.get_nowait()  # remove oldest item
+                                except:
+                                    pass
+                            read_queue.put((angle, count))
+                            print("Angle:", angle, "Count:", count)
+                            timestamp = time.time()
+                            writer.writerow([timestamp, angle, count])
+                            f.flush()
                     except ValueError:
                         print("Bad line:", line)
                 else:
