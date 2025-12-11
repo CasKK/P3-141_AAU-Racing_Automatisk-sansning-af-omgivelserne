d772b1e919968f3f20424b9bca4d94c2da9a835e
PandekageManden <jacoblinderoth1@gmail.com>
2025-12-11 13:15:13 +0100

    Update main.cpp

--- Base/Control/main.cpp
+++ Base/Control/main.cpp
@@ -1,3 +1,252 @@
+Jacob
+trashbagwithlegos
+Online
+
+CasKK — 19-09-2025 09:26
+Fuck jer
+vores projekt
+ahahah
+nooobs
+Heiberg — 19-09-2025 09:26
+🖕
+CasKK — 19-09-2025 09:27
+MUAHAHAHA
+CasKK — 08-10-2025 15:51
+https://youtu.be/BaRdpSXU6EM?si=5KxlP_cOO3FpHjke
+YouTube
+DroneBot Workshop
+NVIDIA Jetson Orin Nano - SSD & MicroSD Setup Guide
+Billede
+Jacob — 10-10-2025 11:08
+Filtype for vedhæng: code
+Rob_2Dof_dynamics.m
+3.96 KB
+CasKK — 13-10-2025 09:17
+planes, 0, 0, 0
+water, 0, 1, 0
+forest, 0, 2, 0
+forest, 0, 3, 0
+forest, 0, 4, 0
+planes, 0, 5, 0
+Udvid
+CorrectTileCrownPoint.txt
+36 KB
+CasKK — 13-10-2025 09:29
+https://www.youtube.com/watch?v=flFbNka62v8
+YouTube
+Kevin Wood | Robotics & AI
+OpenCV Python SIFT Feature Detection (SIFT Algorithm Explained + Code)
+Billede
+CasKK — 13-10-2025 11:00
+with open("Data/AllImages.txt", "w") as f:
+    f.write("")
+Billede
+Jacob — 14-10-2025 16:22
+https://www.omerfarukbay.com.tr/wp-content/uploads/2021/10/BasicEngineeringCircuitAnalysis.pdf
+Heiberg — 15-10-2025 15:33
+% Export_from_maple_FIXED.m
+% Clean MATLAB script that prints tau1 and tau2 and generates tau_rr.m
+% (Converted from the Maple worksheet; ASCII-only, MATLAB-syntax only.)
+
+clear; clc;  % reset
+Udvid
+message.txt
+8 KB
+Jacob — 15-10-2025 16:28
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+Udvid
+message.txt
+11 KB
+Jacob — 16-10-2025 09:13
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Dynamics:
+    def __init__(self, m1, m2, l1, l2, t, q1, q2):
+        self.m1 = m1; self.m2 = m2; self.l1 = l1; self.l2 = l2
+Udvid
+message.txt
+5 KB
+CasKK — 16-10-2025 12:12
+https://www.jetson-ai-lab.com/initial_setup_jon.html
+🚀 Initial Setup Guide - Jetson Orin Nano - NVIDIA Jetson AI ...
+Showcasing generative AI projects that run on Jetson
+https://developer.nvidia.com/embedded/learn/get-started-jetson-orin-nano-devkit#firmware
+NVIDIA Developer
+Jetson Orin Nano Developer Kit Getting Started Guide
+Jetson Orin Nano Developer Kit Getting Started Guide
+Jacob — 20-10-2025 08:52
+clear; clc;
+syms t
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+m1 = 19.5;
+m2 = 9.75;
+l1 = 1;
+Udvid
+message.txt
+7 KB
+CasKK — 02-11-2025 12:50
+https://www.instagram.com/reel/DQe1ZS6jFag/?igsh=MWxmZTY4aTJ4M3psOQ==
+
+robotmandan
+Which one is cool ?!
+
+#Nodemcu #ESP32
+#Arduino #UNO_Q
+#rasspberry #pi5
+#nvidia #jetson_nano
+
+#technology #robotics #creativity #ai #irext #diy
+Likes
+21245
+
+Instagram
+Gosvig24 — 04-11-2025 13:13
+#include <Arduino.h>
+#include <Servo.h>  // Inkluder servo-biblioteket
+
+Servo servoMotor;   // Opret et servo-objekt
+int servoPin = 4;   // Servoen er tilsluttet pin 4 
+
+
+void setup() {
+  servoMotor.attach(servoPin);  // Tilslut servoen til pin 4
+  servoMotor.write(90);         // Start i midterposition
+  delay(1000);
+}
+
+void loop() {
+   // Bevæger servoen fra 0 til 180 grader
+  for (int pos = 90; pos <= 135; pos++) {
+    servoMotor.write(pos);
+    delay(15); // Giver motoren tid til at bevæge sig
+  }
+
+  // Bevæger servoen tilbage fra 180 til 0 grader
+  for (int pos = 135; pos >= 45; pos--) {
+    servoMotor.write(pos);
+    delay(15);
+  }
+
+  for (int pos = 45; pos <= 90; pos++) {
+    servoMotor.write(pos);
+    delay(15);
+  }
+}
+CasKK — 06-11-2025 11:53
+https://www.instagram.com/p/DQo29kYkdZm/?igsh=a2Y3dmRuazc5Nzh6
+
+fakenewsnetwork
+Gm
+Likes
+99993
+
+Instagram
+CasKK — 12-11-2025 14:03
+inputA =   [[-350.13, 50.900, "blue"],
+            [-350.34, 100.90, "blue"],
+            [-350.56, 200.90, "blue"],
+            [-350.13, 250.90, "blue"],
+            [-350.34, 300.90, "blue"],
+            [-350.56, 400.90, "blue"]]
+
+print(inputA)
+
+def someFunction(localInputA):
+    for i, vector in enumerate(localInputA):
+        result = vector[0] + 350
+        localInputA[i] += [result]
+    return localInputA
+
+someVar = someFunction(inputA)
+
+print(inputA)
+Billede
+Heiberg — 12-11-2025 14:05
+What dis?
+CasKK — 12-11-2025 14:05
+it make no sense
+Hvorfor bliver den globale "inputA" ændret?
+!?!?!?
+Billede
+Heiberg — 12-11-2025 14:07
+svært at sige, hvilket modul er det?, er lidt svært at se med din variable navne XD 
+CasKK — 12-11-2025 14:07
+Det er outputtet...
+Det screenshot er hele coden.
+HELE
+Det er en test kode.
+Heiberg — 12-11-2025 14:07
+Output funktionen af hvilket modul?
+CasKK — 12-11-2025 14:08
+Det er ligegyldigt! Det handler om at python af en eller anden grund ændre på mine globale variable!
+Det skal den ikke!
+Heiberg — 12-11-2025 14:10
+Har lige kigget på det men kan ikke spotte noget
+galt
+CasKK — 14-11-2025 15:03
+https://www.youtube.com/watch?v=jvPPXbo87ds
+YouTube
+Freya Holmér
+The Continuity of Splines
+Billede
+don't mind
+Heiberg — 14-11-2025 21:34
+Mange tak for advarslen aalborg forsyning
+Billede
+CasKK — 18-11-2025 10:10
+inputA =    [[-350.13, 50.90, "blue"],
+            [-350.34, 100.90, "blue"],
+            [-350.56, 200.90, "blue"],
+            [-350.13, 250.90, "blue"],
+            [-350.34, 300.90, "blue"],
+            [-350.56, 400.90, "blue"]]
+
+print(inputA)
+
+def someFunction(localInputA):
+    for i, vector in enumerate(localInputA):
+        result = vector[0] + 350
+        localInputA[i] += [result]
+    return localInputA
+
+someVar = someFunction(inputA)
+
+print(inputA)
+print(someVar)
+CasKK — 20-11-2025 09:01
+Billede
+Jacob — 26-11-2025 08:54
+Filtype for vedhæng: archive
+Grove IMU 9DOF ICM20600 AK09918.zip
+84.14 KB
+Heiberg — 26-11-2025 12:36
+
+#include <Arduino.h>
+#include <Servo.h>
+#include "ICM20600.h"
+#include <Wire.h>
+#include <math.h>
+Udvid
+message.txt
+5 KB
+CasKK — 02-12-2025 10:00
+https://www.youtube.com/watch?v=dklwlNqBvRI
+YouTube
+Elektronika - FE
+Altium intro #26: Copper plane
+Billede
+CasKK — 05-12-2025 08:26
+output_queue.put((oldPointsB, oldPointsY))
+CasKK — 05-12-2025 10:49
+Filtype for vedhæng: unknown
+Sheet2.SchDoc
+84.50 KB
+CasKK — 10:48
 #include <Arduino.h>
 #include <Servo.h>
 #include "ICM20600.h"
@@ -31,7 +280,112 @@ unsigned long time;
 bool newData = false;
 float wheelAngle;
 float wheelAngleTemp;
-float desiredSpeed = 0.8f;  // m/s
+float desiredSpeed = 0.4f;  // m/s
+unsigned long lastTime = 0;
+ICM20600 icm20600(true);
+
+
+// -----------------------------------------------------------
+// Interrupt Service Routine
+// -----------------------------------------------------------
+void encoderISR() {
+  unsigned long now = micros();
+  if (now - lastPulseTime > 1000) { // debounce: ignore pulses <1ms apart
+    encoderCount++;
+    lastPulseTime = now;
+  }
+}
+
+// -----------------------------------------------------------
+// Proportional Speed Controller (returns microseconds)
+// -----------------------------------------------------------
+int P_control(float desired_speed, float current_rpm) {
+  const float Kp = 0.2f;           // Reduced gain for stability
+  const float deadband = 3.0f;     // RPM deadband to avoid chasing noise
+  const int min_us = 1600;        // Spin start point
+  const int max_us = 2000;
+
+  static int lastOutput = min_us + 50;
+
+  float desired_rpm = (desired_speed * 60.0f) / WHEEL_CIRCUMFERENCE;
+  float error = desired_rpm - current_rpm;
+
+  if (abs(error) < deadband) {
+    return lastOutput;
+  }
+
+  float throttle_delta = Kp * error; // No extra scaling
+
+  int output = lastOutput + throttle_delta;
+
+  output = constrain(output, min_us, max_us);
+  lastOutput = output;
+  if (debug == true){  
+    Serial.print(">P_control Output:");
+    Serial.println(output);
+  }
+
+  return output;
+}
+
+
+
+void ReadSerial() {
+    if (Serial.available() > 0 && newData == false) {
+        wheelAngleTemp = Serial.read();   // 0–255 direkte
+        wheelAngleTemp = constrain(wheelAngleTemp, 0, 180);
+        newData = true;
+    }
+}
+
+
+
+// -----------------------------------------------------------
+// Setup
+// -----------------------------------------------------------
+void setup() {
+  Wire.begin();
+  Wire.setClock(400000);
+  icm20600.initialize();
+... (125 linjer linjer tilbage)
+Fold ud
+message (1).txt
+6 KB
+﻿
+#include <Arduino.h>
+#include <Servo.h>
+#include "ICM20600.h"
+#include <Wire.h>
+#include <math.h>
+
+// Encoder and Wheel Parameters
+
+#define ENCODER_PIN 2
+#define PULSES_PER_REV 100       // Counting both edges (50 holes × 2)
+
+const float WHEEL_CIRCUMFERENCE = 0.5776f;  // meters
+const bool debug = false;
+unsigned long lastCalcTime = 0;
+unsigned long lastPulseTime = 0;
+
+volatile long encoderCount = 0;
+long lastEncoderCount = 0;
+int val = 90;
+float rpm = 0.0f;
+float speed_mps = 0.0f;
+
+Servo esc;
+Servo steering;
+
+// Gyro variables
+unsigned long t_prev = 0;
+float angle = 0.0f;
+float gyro_offset = 0.0f;
+unsigned long time;
+bool newData = false;
+float wheelAngle;
+float wheelAngleTemp;
+float desiredSpeed = 0.4f;  // m/s
 unsigned long lastTime = 0;
 ICM20600 icm20600(true);
 
@@ -102,15 +456,15 @@ void setup() {
   Serial.begin(115200);
     
    // Kalibrér gyro offset
-  Serial.println("Calibrating gyro...");
+  //Serial.println("Calibrating gyro...");
   int sum = 0;
   for (int i = 0; i < 1000; i++) {
     sum += icm20600.getGyroscopeZ();
     delay(2);
   }
   gyro_offset = sum / 1000.0f;
-  Serial.print("Gyro offset: ");
-  Serial.println(gyro_offset);
+  //Serial.print("Gyro offset: ");
+  //Serial.println(gyro_offset);
   t_prev = micros();
 
   pinMode(ENCODER_PIN, INPUT);
@@ -119,8 +473,8 @@ void setup() {
   steering.attach(10);
   steering.write(val);
   delay(500);
-  Serial.println("Arduino Is ready :)");
-  delay(3000);
+  //Serial.println("Arduino Is ready :)");
+  delay(10000);
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), encoderISR, CHANGE);
 }
 
@@ -140,7 +494,7 @@ float Gyroscope(){
   unsigned long t_now = 0;
   t_now = micros();       
   float dt = (t_now - t_prev)/ 1e6;
-  Serial.println((t_now - t_prev),10);
+  //Serial.println((t_now - t_prev),10);
   t_prev = t_now;
   float gyroZ = icm20600.getGyroscopeZ() - gyro_offset; 
   angle += gyroZ * dt; 
@@ -151,7 +505,7 @@ float Gyroscope(){
     Serial.println(angle);
   }
 
-  return M_PI/180 * angle;
+  return angle;
 }
 
 void Encoder(){
@@ -177,10 +531,11 @@ void Encoder(){
 
     // Update throttle only at RPM refresh
     int throttle_us = P_control(desiredSpeed, rpm);
+    throttle_us = 1000; // Neutral for testing
     esc.writeMicroseconds(throttle_us);
 
     if (debug == true){
-      // Debug output
+      //Debug output
       Serial.print(">encoderCount:");
       Serial.println(snapshot);
       Serial.print(">Rounds:");
@@ -216,9 +571,9 @@ void loop() {
   Encoder();
   float angleValue = Gyroscope();
   if (time - lastTime >= 33000){
-    Serial.print(angleValue);
+    Serial.print((int)angleValue);
     Serial.print(", ");
-    Serial.println(encoderCount);
+    Serial.println((long)encoderCount);
     lastTime = time;
   }
 }
