68d944a159f3389126890946a780ba2a2c6a534b
Jetson420 <ckkr@student.aau.dk>
2025-12-12 08:51:06 +0100

    hej67

--- Base/M-1.2.1_test_angle.py
+++ Base/M-1.2.1_test_angle.py
@@ -5,6 +5,6 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
 
 while True:
     message = int(input())
-    print(f"messageOutToArduino: {message}")
     message = bytes([message])
     ser.write(message)
+    print(f"messageOutToArduino: {message}")
