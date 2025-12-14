2327306daf4d1fe206158544b82ab4ce71bc7869
CasKK <82200666+CasKK@users.noreply.github.com>
2025-12-14 15:17:25 +0100

    asdasd

--- Base/encoderTest/sketch_nov14a/sketch_nov14a.ino
+++ Base/encoderTest/sketch_nov14a/sketch_nov14a.ino
@@ -2,6 +2,7 @@
 #include <Servo.h>
 
 Servo myServo;  // create a servo object
+Servo myServo1;  // create a servo object
 
 
 
@@ -18,21 +19,32 @@ void encoderAFunc();
 void setup() {
   Serial.begin(115200);
   pinMode(ENCODER_A, INPUT);
-  myServo.attach(6);
+  myServo.attach(11, 1000, 2000);
+  myServo1.attach(10);
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderAFunc, RISING);
 }
 
 void loop() {
-  Serial.print("  >DigitalValue:");
+  // Serial.print("  >DigitalValue:");
   Serial.print(digitalRead(ENCODER_A));
-  Serial.print("  >EncoderValue:");
+  Serial.print(",");
+  //Serial.print("  >EncoderValue:");
   Serial.println(encoder_Value);
-  myServo.write(135);
+  myServo.writeMicroseconds(1750);
+  myServo1.write(90);
   delay(1);
+  while (encoder_Value > 2000){
+    myServo.writeMicroseconds(1750);
+    Serial.print(digitalRead(ENCODER_A));
+    Serial.print(",");
+    //Serial.print("  >EncoderValue:");
+    Serial.println(encoder_Value);
+    delay(1000);
+  }
 }
 
 void encoderAFunc() {
-  if (micros() - encoderTime > 500) {
+  if (micros() - encoderTime > 1000) {
     encoder_Value += 1;
   }
   encoderTime = micros();
