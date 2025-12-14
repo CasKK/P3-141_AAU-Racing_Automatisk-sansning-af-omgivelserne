70acb07767143643a1074aa252b17d6ec0cdd721
CasKK <82200666+CasKK@users.noreply.github.com>
2025-12-14 16:33:52 +0100

    Renaming for overview

--- Base/main_Plot_Log.py
+++ Base/main_Plot_Log.py
@@ -1,8 +1,8 @@
 from multiprocessing import Process, Queue
 import time
 #from M111_cam import ConeDetectionv5
-from M113_position import Coordinates_test4
-from M121_logic import whatEver1
+from M113_position import Coordinates_Plot_Log
+from M121_logic import M121_logic_plot
 #import serial_manager_test
 
 if __name__ == "__main__":
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     q_serial_write = Queue() # Serial que til afsendelse "kun på m121"
     
     #m111 = Process(target=ConeDetectionv5.run, args=(q_m111_to_m113,))
-    m113 = Process(target=Coordinates_test4.run, args=(q_m113_to_m121,)) # q_m111_to_m113,  q_serial_read
-    m121 = Process(target=whatEver1.run, args=(q_m113_to_m121, q_serial_write))
+    m113 = Process(target=Coordinates_Plot_Log.run, args=(q_m113_to_m121,)) # q_m111_to_m113,  q_serial_read
+    m121 = Process(target=M121_logic_plot.run, args=(q_m113_to_m121, q_serial_write))
     #serial = Process(target=serial_manager_test.run, args=(q_serial_read, q_serial_write))
 
     #m111.start()
--- Base/main_test4.py
+++ Base/main_test4.py
@@ -1,8 +1,8 @@
 from multiprocessing import Process, Queue
 import time
 from M111_cam import ConeDetectionv5
-from M113_position import Coordinates_test5
-from M121_logic import whatEver1
+from M113_position import Coordinates_test4
+from M121_logic import M121_logic
 import serial_manager
 
 if __name__ == "__main__":
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     q_serial_write = Queue() # Serial que til afsendelse "kun på m121"
     
     m111 = Process(target=ConeDetectionv5.run, args=(q_m111_to_m113,))
-    m113 = Process(target=Coordinates_test5.run, args=(q_m113_to_m121, q_serial_read)) # q_m111_to_m113, 
-    m121 = Process(target=whatEver1.run, args=(q_m113_to_m121, q_serial_write))
+    m113 = Process(target=Coordinates_test4.run, args=(q_m113_to_m121, q_serial_read)) # q_m111_to_m113, 
+    m121 = Process(target=M121_logic.run, args=(q_m113_to_m121, q_serial_write))
     serial = Process(target=serial_manager.run, args=(q_serial_read, q_serial_write))
 
     m111.start()
